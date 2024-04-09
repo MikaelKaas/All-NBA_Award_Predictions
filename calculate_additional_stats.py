255cd94bd3c340156dac07ad950f0a588f1782bf
Mikael Kaas <mka189@sfu.ca>
2024-04-08 22:25:36 -0700

    Created calculate_additional_stats.py

---
+++ calculate_additional_stats.py
@@ -0,0 +1,6 @@
+# here I will calculate additional advanced stats with out data
+# these will be popular stats that aren't given by the nba api but can be calculated with what we do have
+# these will be important stats for comparing players which will be valuable for our predictions
+# stats that I want to calculate:
+# - player efficiency rating PER
+# - win shares WS
